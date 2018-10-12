dba533d3693dde50e0bad8ee57410414d3e95cf3
Christian Salgård Svinding <chri30cm@edu.eal.dk>
2018-10-12 08:40:25 +0200

    445634

--- SmartMenuLibrary/Bindings.cs
+++ SmartMenuLibrary/Bindings.cs
@@ -10,48 +10,48 @@ namespace SmartMenuLibrary
         {
             if (menu_id == "menu1")
             {
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 double input = double.Parse(Console.ReadLine());
-                Console.WriteLine(" + ");
+                Console.WriteLine("+");
                 double input2 = double.Parse(Console.ReadLine());
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 Console.WriteLine(input + " + " + input2 + " = " + Calculator.Add(input, input2));
             }
 
             else if (menu_id == "menu2")
             {
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 double input = double.Parse(Console.ReadLine());
-                Console.WriteLine(" - ");
+                Console.WriteLine("-");
                 double input2 = double.Parse(Console.ReadLine());
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 Console.WriteLine(input + " - " + input2 + " = " + Calculator.Subtract(input, input2));
             }
 
             else if (menu_id == "menu3")
             {
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 double input = double.Parse(Console.ReadLine());
-                Console.WriteLine(" * ");
+                Console.WriteLine("*");
                 double input2 = double.Parse(Console.ReadLine());
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 Console.WriteLine(input + " + " + input2 + " = " + Calculator.Multiply(input, input2));
             }
 
             else if (menu_id == "menu4")
             {
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 double input = double.Parse(Console.ReadLine());
-                Console.WriteLine(" / ");
+                Console.WriteLine("/");
                 double input2 = double.Parse(Console.ReadLine());
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 Console.WriteLine(input + " / " + input2 + " = " + Calculator.Divide(input, input2));
             }
 
             else if (menu_id == "menu5")
             {
                 List<double> numbers = new List<double>();
-                Console.WriteLine(" ");
+                Console.WriteLine();
 
                 bool Looprunning = true;
                 while (Looprunning != false)
@@ -75,7 +75,7 @@ namespace SmartMenuLibrary
             else if (menu_id == "menu6")
             {
                 List<double> numbers = new List<double>();
-                Console.WriteLine(" ");
+                Console.WriteLine();
 
                 bool Looprunning = true;
                 while (Looprunning != false)
@@ -99,7 +99,7 @@ namespace SmartMenuLibrary
             else if (menu_id == "menu7")
             {
                 List<double> numbers = new List<double>();
-                Console.WriteLine(" ");
+                Console.WriteLine();
 
                 bool Looprunning = true;
                 while (Looprunning != false)
@@ -123,7 +123,7 @@ namespace SmartMenuLibrary
             else if (menu_id == "menu8")
             {
                 List<double> numbers = new List<double>();
-                Console.WriteLine(" ");
+                Console.WriteLine();
 
                 bool Looprunning = true;
                 while (Looprunning != false)
@@ -146,11 +146,11 @@ namespace SmartMenuLibrary
 
             if (menu_id == "menu9")
             {
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 double input = double.Parse(Console.ReadLine());
-                Console.WriteLine(" % ");
+                Console.WriteLine("%");
                 double input2 = double.Parse(Console.ReadLine());
-                Console.WriteLine(" ");
+                Console.WriteLine();
                 Console.WriteLine(input + " % " + input2 + " = " + Calculator.Procent(input, input2));
             }
         }
